22095c980a44e401ee59f52ca21731231e4c857e
Y Nasr <yoyo.monem22@gmail.com>
2022-11-28 21:25:41 +0100

    Update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import MarkdownEditor from "./MarkdownEditor";
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.props.type = String;
+    }
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+    sendErrorStatistics() {
+        window.opener = null;
+        window.open("https://github.com/yoyomonem/jsx-md-edit/issues/new?assignees=yoyomonem&labels=bug&template=bug_report.md&title=%5BREACT+BUG%5D+The+React+app+itself");
+    }
+    componentDidCatchError() {
+        // You can also log the error to an error reporting service
+        sendErrorStatistics();
+    }
     render() {
         return (
             <React.StrictMode>
                 <h1>Markdown Editor (React JSX)</h1>
                 <MarkdownEditor />
+                <p>Type: {this.props.type}</p>
             </React.StrictMode>
         );
     }
